fbf2762ce45259dddda2ca654372183943ebe2e8
GaryNLOL <garypastel@gmail.com>
2021-09-23 17:28:52 -0500

    Added sponsor prospectus

---
+++ static/js/links.js
@@ -0,0 +1,4 @@
+document.getElementById('sponsors-button').addEventListener('click',
+    function(){
+        window.open('https://drive.google.com/file/d/1dZLfdYeRiNUdpIZu6abrHnSioDC2V1ol/view?usp=sharing','_blank');
+    });
