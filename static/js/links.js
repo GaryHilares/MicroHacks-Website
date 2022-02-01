3ab666e6ed7f53af771ec3482a7b4cf7ca75b732
GaryNLOL <dev.garynlol@gmail.com>
2022-01-31 21:55:58 -0500

    Applied code formatter

--- static/js/links.js
+++ static/js/links.js
@@ -1,9 +1,9 @@
 
-function linkButton(id,link){
+function linkButton(id, link) {
     document.getElementById(id).addEventListener('click',
-    function(){
-        window.open(link,'_blank');
-    });
+        function () {
+            window.open(link, '_blank');
+        });
 }
 
 linkButton('sponsors-button', 'https://drive.google.com/file/d/1dZLfdYeRiNUdpIZu6abrHnSioDC2V1ol/view?usp=sharing');
