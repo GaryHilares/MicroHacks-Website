ede931fc9d0c049fa6afe37e4553a35ea022c013
GaryNLOL <dev.garynlol@gmail.com>
2022-06-08 17:11:26 -0500

    Added Discord redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.com/invite/GaPHHqANYq',
+        permanent: false
+      }
+    ];
+  }
+};
 
 module.exports = nextConfig
