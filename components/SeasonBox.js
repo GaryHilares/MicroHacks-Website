e7fa8a847427ad2e5c6d1724f7fc202d89cb5f14
GaryNLOL <dev.garynlol@gmail.com>
2022-11-25 14:09:56 -0500

    Fixed season boxes

--- components/SeasonBox.js
+++ components/SeasonBox.js
@@ -1,11 +1,25 @@
-function SeasonBox() {
+function SeasonBox(props) {
+    const testProps = [
+        { name: "µHacks ZERO", date: "January 2022", url: "https://microhacks.devpost.com/" },
+        { name: "µHacks 2023", date: "May 2023", url: "discord.com/" }
+    ]
     return (
-        <a href="https://microhacks.devpost.com/">
-            <div style={{ border: "2px solid black" }}>
-                <h1 style={{ fontWeight: "bold" }}>µHacks ZERO!</h1>
-                <span>January 2022</span>
-            </div>
-        </a>
+        <div>
+            {testProps.map(season => {
+                return (
+                    <div style={{ border: "2px solid black", padding: "10px", marginBottom: "10px", overflow: "hidden" }}>
+                        <div style={{ width: "80%", display: "inline-block" }}>
+                            <h1 style={{ fontWeight: "bold" }}>{season.name}</h1>
+                            <span>{season.date}</span>
+                        </div>
+                        <a href={season.url}>
+                            <div style={{ margin: "0", float: "right", borderRadius: "50%", width: "50px", height: "50px", backgroundColor: "black" }}>
+                            </div>
+                        </a>
+                    </div>
+                );
+            })}
+        </div >
     );
 }
 
